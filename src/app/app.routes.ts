17eea3e40848e7fb8891d95ce253f689e9888182
Shruti Jaiswal <shruti.jaiswal@softude.com>
2024-09-23 18:12:55 +0530

    Initial commit with registration form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'sign-up',component:SignUpComponent},
+    {path:'sign-in',component:SignInComponent}
+
+];
